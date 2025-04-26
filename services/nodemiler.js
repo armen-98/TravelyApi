72069a62635edc662c24108e087276574b7c0b05
admin <matevosyan.2011@gmail.com>
2025-04-26 18:07:38 +0400

    added error message sender

---
+++ services/nodemiler.js
@@ -0,0 +1,52 @@
+const nodemailer = require('nodemailer');
+
+async function sendEmail({ to, subject, text, html }) {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: false,
+      service: process.env.MAIL_SERVICE,
+      ignoreTLS: true,
+      socketTimeout: 10000,
+      requireTLS: true,
+      auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: '"WanderGid api 👻" <WanderGid>',
+      to,
+      subject,
+      text,
+      html,
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    return info;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+}
+
+const sendErrorEmail = async (error) => {
+  try {
+    console.log('error', error);
+    await sendEmail({
+      to: ['matevosyan.2011@gmail.com', 'armann.davtyan@gmail.com'],
+      subject: 'API error!',
+      text: error.message,
+      html: `<h1>${error}</p>`,
+    });
+  } catch (error) {
+    console.log('sendErrorEmail function catch', error);
+  }
+};
+
+module.exports = {
+  sendEmail,
+  sendErrorEmail,
+};
