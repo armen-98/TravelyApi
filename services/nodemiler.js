e275c82c7ae1a1f5d6b16d1af283d5f6eb39f001
admin <matevosyan.2011@gmail.com>
2025-10-22 21:33:18 +0400

    checking email issue

--- services/nodemiler.js
+++ services/nodemiler.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 async function sendEmail({ to, subject, text, html, attachments = [], cc }) {
   try {
-    console.log('process.env.MAIL_HOST', process.env.MAIL_HOST);
-    console.log('process.env.MAIL_PORT', process.env.MAIL_PORT);
-    console.log('process.env.MAIL_SERVICE', process.env.MAIL_SERVICE);
-    console.log('process.env.MAIL_USERNAME', process.env.MAIL_USERNAME);
-    console.log('process.env.MAIL_PASSWORD', process.env.MAIL_PASSWORD);
+    console.error('process.env.MAIL_HOST', process.env.MAIL_HOST);
+    console.error('process.env.MAIL_PORT', process.env.MAIL_PORT);
+    console.error('process.env.MAIL_SERVICE', process.env.MAIL_SERVICE);
+    console.error('process.env.MAIL_USERNAME', process.env.MAIL_USERNAME);
+    console.error('process.env.MAIL_PASSWORD', process.env.MAIL_PASSWORD);
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
